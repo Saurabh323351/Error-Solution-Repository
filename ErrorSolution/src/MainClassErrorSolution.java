5afef0b6742ab962dac6cf362ac5272130eb25c5
Saurabh323351 <singh.saurabh3333@gmail.com>
2018-10-30 21:11:06 +0530

    could not find or loads main class error solved

---
+++ ErrorSolution/src/MainClassErrorSolution.java
@@ -0,0 +1,16 @@
+
+public class MainClassErrorSolution {
+
+	/**
+	 * 
+	 * First check this:
+Right click on your project>>properties>>JavaBuildPath>>Libraries.
+If there are any red flags, remove and attach the library again.
+Furthermore, check your .classpath file, it should have correct paths to your main.
+Finally, you can click Project -> Clean.
+I also suggest using Maven, as it helps with those type of issues
+	 * 
+	 * 
+	 */
+	
+}
